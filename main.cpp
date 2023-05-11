0a8499ff853cf5b06661e4ee567a2b77f7e346df
Onurcanblkn <132472660+Onurcanblkn@users.noreply.github.com>
2023-05-11 23:58:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
-#include <string>
-#include <cstdlib>
-#include "textAnimator.h"
+#include <ctime>
 
-using namespace std;
+int main() {
+    std::srand(std::time(nullptr));
 
-int main()
-{
-    print_animated("This text will be printed in 1.5 seconds!", 1.5);
+    const int numberofquestions = 5;
+    int answers[numberofquestions] = {4, 7, 2, 9, 5};
+    int point = 0;
+
+    std::cout << "Welcome to the game!\n";
+    std::cout << "You have a total of 10 seconds for each question.\n";
+    std::cout << "Be ready!\n";
+
+    for (int i = 0; i < numberofquestions; i++) {
+        std::cout << "Question " << i+1 << ": " << "10 * " << i+1 << " = ";
+        int answer;
+        std::cin >> answer;
+
+        std::clock_t starttime = std::clock();
+        while ((std::clock() - starttime) / (double)CLOCKS_PER_SEC < 10.0) {
+            if (answer == answers[i]) {
+                std::cout << "The correct answer!\n";
+                point++;
+                break;
+            }
+        }
+        if (answer != answers[i]) {
+            std::cout << "Time is up! The correct answer was " << answers[i] << ".\n";
+        }
+    }
+
+    std::cout << "Game over. Total points: " << point << "\n";
     return 0;
 }
