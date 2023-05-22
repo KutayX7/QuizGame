075b81a90fefac9ea1e8da1284c9425b17124e91
KutayX7 <103666634+KutayX7@users.noreply.github.com>
2023-05-22 18:09:55 +0300

    Test

--- main.cpp
+++ main.cpp
@@ -11,14 +11,23 @@ using namespace std;
 
 int main()
 {
+    clear_screen(); // Always call this first
     print_animated("\033[1;33mWelcome to The Quiz Game! \n\033[0m\n", 1.0);
+    enter_to_continue();
     std::list<Question> qlist = getallquestions(true);
     int size = qlist.size();
     for (int i;i < size;i++)
     {
         Question q = qlist.front();
         qlist.pop_front();
-        q.printq();
+        bool r = q.prompt();
+        if (!r)
+        {
+            clear_screen();
+            cout << "Game over! You're a failure! \n";
+            enter_to_continue();
+            break;
+        }
     }
     return 0;
 }
